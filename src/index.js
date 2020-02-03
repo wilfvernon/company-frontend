f832ac4934007be5fe83bb57e72f388d309f97bd
github username <github email address>
2020-02-03 15:13:55 +0000

    removed redux devtools

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer, 
   compose(
-    applyMiddleware(thunk), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
